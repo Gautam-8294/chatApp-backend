b5709c5e3d2d8854c7872d96c93ece2e31d49715
Gautam Kumar <9905614126gk@gmail.com>
2023-07-20 23:20:06 +0530

    cors1

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors({
     // origin: ["http://localhost:3000"],
     // origin: ["https://chat-app-frontend-swart.vercel.app/"],
     // origin: ["https://vercel.live/link/chat-app-frontend-git-main-gautam-8294.vercel.app?via=deployment-domains-list-branch"],
-    origin: ["https://chat-app-frontend-gautam-8294.vercel.app/"],
+    // origin: ["https://chat-app-frontend-gautam-8294.vercel.app/"],
     origin: ["https://chat-app-frontend-swart.vercel.app"],
     methods: ["GET","POST"],
     credentials: true
